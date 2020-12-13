60910c9f8af18234cad2fc24896b2316645acf62
Juan Gonzalez <jgonzalegarc.iacs@aragon.es>
2020-12-13 18:07:54 +0100

    Added ROSE oversampling tests

--- sampling_manipulations.R
+++ sampling_manipulations.R
@@ -28,6 +28,44 @@ write_dta(Farvadin_2_balanced_over_train, "Farvadin_2_balanced_over_train80.dta"
 saveRDS(Farvadin_2_balanced_over_test, "Farvadin_2_balanced_over_test_20.Rda")
 write_dta(Farvadin_2_balanced_over_test, "Farvadin_2_balanced_over_test_20.dta")
 
+# Oversampling ROSE ----
+
+library(ROSE)
+
+Farvadin_2 %>% 
+  select(Outcome,
+         PT,
+         Neutrophilspercent,
+         lymphocytespercent,
+         CatHaematocrit,
+         Age,
+         CRP,
+         Respiratorydistress1,
+         ASTALTRatio,
+         APTT,
+         BloodUreaNitrogen) %>% 
+  # filter(complete.cases(.))  %>% 
+  mutate(Outcome = recode(Outcome, "0" = "ALIVE", "1" = "DEATH")) %>% 
+  fastDummies::dummy_columns(select_columns = c("CatHaematocrit"), 
+                             ignore_na = T,
+                             remove_selected_columns = T) %>% 
+  mutate_at(vars(matches(c("Respiratorydistress1","CatHaematocrit"))), as.factor) -> Farvadin_2_Mixmodel
+
+Farvadin_2_balanced_ROSE <- 
+  Farvadin_2_Mixmodel %>% ROSE(Outcome ~ ., data=., N=nrow(Farvadin_2), seed=3)
+
+table(Farvadin_2_balanced_ROSE$data$Outcome)
+prop.table(table(Farvadin_2_balanced_ROSE$data$Outcome))
+
+trainIndex_ROSE <- createDataPartition(as.factor(Farvadin_2_balanced_ROSE$data$Outcome), p=0.80, list=FALSE)
+Farvadin_2_balanced_ROSE_train <- Farvadin_2_balanced_ROSE$data[as.vector(trainIndex_ROSE),]
+Farvadin_2_balanced_ROSE_test <- Farvadin_2_balanced_ROSE$data[-as.vector(trainIndex_ROSE),]
+
+saveRDS(Farvadin_2_balanced_ROSE_train, "Farvadin_2_balanced_ROSE_train_80.Rda")
+write_dta(Farvadin_2_balanced_ROSE_train, "Farvadin_2_balanced_ROSE_train80.dta")
+saveRDS(Farvadin_2_balanced_ROSE_test, "Farvadin_2_balanced_ROSE_test_20.Rda")
+write_dta(Farvadin_2_balanced_ROSE_test, "Farvadin_2_balanced_ROSE_test_20.dta")
+
 # Undersampling ----
 
 Farvadin_2_balanced_under <- downSample(Farvadin_2 %>% select(-Outcome), Farvadin_2$Outcome, yname="Outcome")
